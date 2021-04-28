36af18a8fbd1a0555bf816ec4ac16c02098110d9
Shin Seunghun <seunghunee@gmail.com>
2021-04-28 09:52:26 +0900

    Layout을 material-ui로 재구현

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,15 +1,9 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
+import CssBaseline from "@material-ui/core/CssBaseline"
+import Container from "@material-ui/core/Container"
 
 import Header from "./header"
-import "./layout.css"
 
 const Layout: React.FC = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -24,25 +18,11 @@ const Layout: React.FC = ({ children }) => {
 
   return (
     <>
+      <CssBaseline />
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+      <Container maxWidth="sm">
         <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `2rem`,
-          }}
-        >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
+      </Container>
     </>
   )
 }
