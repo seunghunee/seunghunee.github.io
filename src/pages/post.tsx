3475dc91966cf99abab8e308e2881ab78217d4d1
Shin Seunghun <seunghunee@gmail.com>
2021-04-29 16:32:55 +0900

    install: gatsby-remark-prismjs

--- src/pages/post.tsx
+++ src/pages/post.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { PageProps, graphql } from "gatsby"
+import "prismjs/themes/prism-tomorrow.css"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
