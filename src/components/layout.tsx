88cc7fde35f1892cbf0ee964a35b4ea6d09f0a79
Shin Seunghun <seunghunee@gmail.com>
2021-04-30 10:02:44 +0900

    change: 기본레이아웃의 가장 위아래에 여백을 추가

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import CssBaseline from "@material-ui/core/CssBaseline"
 import Container from "@material-ui/core/Container"
+import { makeStyles } from "@material-ui/core/styles"
 
 import Header from "./header"
 
@@ -16,11 +17,12 @@ const Layout: React.FC = ({ children }) => {
     }
   `)
 
+  const classes = useStyles()
   return (
     <>
       <CssBaseline />
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <Container maxWidth="md">
+      <Header siteTitle={data.site.siteMetadata.title} />
+      <Container className={classes.main} maxWidth="md">
         <main>{children}</main>
       </Container>
     </>
@@ -28,3 +30,10 @@ const Layout: React.FC = ({ children }) => {
 }
 
 export default Layout
+
+const useStyles = makeStyles(theme => ({
+  main: {
+    marginTop: theme.spacing(6),
+    marginBottom: theme.spacing(6),
+  },
+}))
