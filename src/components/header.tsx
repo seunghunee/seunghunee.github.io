4a45692e6b26b40d13f2196e59d48a66f40a29a1
Shin Seunghun <seunghunee@gmail.com>
2021-04-30 21:55:31 +0900

    change: 헤더의 App Bar디자인 변경

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,27 +1,40 @@
 import * as React from "react"
 import AppBar from "@material-ui/core/AppBar"
 import Toolbar from "@material-ui/core/Toolbar"
+import { makeStyles } from "@material-ui/core/styles"
+import Typography from "@material-ui/core/Typography"
 
 import Link from "./link"
 
 interface Props {
-  siteTitle?: string
+  siteTitle: string
 }
 
-const Header: React.FC<Props> = ({ siteTitle = "" }) => (
-  <AppBar position="sticky">
-    <Toolbar>
-      <Link
-        to="/"
-        style={{
-          color: `white`,
-          textDecoration: `none`,
-        }}
-      >
-        {siteTitle}
-      </Link>
-    </Toolbar>
-  </AppBar>
-)
+const Header: React.FC<Props> = ({ siteTitle }) => {
+  const classes = useStyles()
+  return (
+    <AppBar
+      position="relative"
+      elevation={0}
+      style={{
+        background: "linear-gradient(45deg, #00C9B7 30%, #00B6D0 90%)",
+      }}
+    >
+      <Toolbar>
+        <Link to="/">
+          <Typography className={classes.siteTitle}>{siteTitle}</Typography>
+        </Link>
+      </Toolbar>
+    </AppBar>
+  )
+}
 
 export default Header
+
+const useStyles = makeStyles(theme => ({
+  siteTitle: {
+    color: "#E8F3F1",
+    fontWeight: "bold",
+    fontSize: "1.6em",
+  },
+}))
