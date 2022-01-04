73d5ebbadff21679e38bf2d73b23dab898173e57
Shin Seunghun <seunghunee@gmail.com>
2022-01-04 23:46:31 +0900

    Uninstall: gatsby-remark-emoji

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
     {
       resolve: `gatsby-transformer-remark`,
       options: {
-        plugins: [`gatsby-remark-emoji`, `gatsby-remark-prismjs`],
+        plugins: [`gatsby-remark-prismjs`],
       },
     },
     `gatsby-plugin-image`,
