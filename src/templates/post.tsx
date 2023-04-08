06c96b4a74ba91de8986458bba47cd376bed0c40
Shin Seunghun <seunghunee@gmail.com>
2023-04-08 11:44:18 +0900

    Fix styling details

--- src/templates/post.tsx
+++ src/templates/post.tsx
@@ -6,97 +6,94 @@ import "prismjs/themes/prism-tomorrow.css"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const PREFIX = "Post"
+const PostMain = styled("div")(({ theme }) => ({
+  [theme.breakpoints.up(680)]: {
+    width: 680,
+    margin: "auto",
+  },
+}))
 
-const classes = {
-  post: `${PREFIX}-post`,
-  body: `${PREFIX}-body`,
-}
-
-const StyledLayout = styled(Layout)(({ theme }) => ({
-  [`& .${classes.post}`]: {
-    [theme.breakpoints.up(680)]: {
-      width: 680,
-      margin: "auto",
+const Body = styled("div")(({ theme }) => ({
+  color: "#222",
+  "& a": {
+    color: "#00B6D0",
+  },
+  "& img": {
+    width: "100%",
+  },
+  '& pre[class*="language-"]': {
+    borderRadius: "0.4em",
+    [theme.breakpoints.up("sm")]: {
+      fontSize: "1.15rem",
+    },
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "1rem",
     },
   },
-
-  [`& .${classes.body}`]: {
-    color: "#222",
-    "& a": {
-      color: "#00B6D0",
+  "& code.language-text": {
+    color: "#444",
+    backgroundColor: "#EEE",
+    padding: ".2em",
+  },
+  "& h1": {
+    [theme.breakpoints.up("sm")]: {
+      fontSize: "3rem",
     },
-    "& img": {
-      width: "100%",
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "2rem",
     },
-    '& pre[class*="language-"]': {
-      borderRadius: "0.4em",
+  },
+  "& h2": {
+    marginTop: theme.spacing(6),
+    [theme.breakpoints.up("sm")]: {
+      fontSize: "2rem",
     },
-    "& code.language-text": {
-      color: "#444",
-      backgroundColor: "#EEE",
-      padding: ".2em",
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "1.5rem",
     },
-    "& h1": {
-      [theme.breakpoints.up("xs")]: {
-        fontSize: "3rem",
-      },
-      [theme.breakpoints.down("xs")]: {
-        fontSize: "2rem",
-      },
+  },
+  "& h3": {
+    [theme.breakpoints.up("sm")]: {
+      fontSize: "1.5rem",
     },
-    "& h2": {
-      marginTop: theme.spacing(6),
-      [theme.breakpoints.up("xs")]: {
-        fontSize: "2rem",
-      },
-      [theme.breakpoints.down("xs")]: {
-        fontSize: "1.5rem",
-      },
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "1.3rem",
     },
-    "& h3": {
-      [theme.breakpoints.up("xs")]: {
-        fontSize: "1.5rem",
-      },
-      [theme.breakpoints.down("xs")]: {
-        fontSize: "1.3rem",
-      },
+  },
+  "& p": {
+    color: "#444",
+    [theme.breakpoints.up("sm")]: {
+      fontSize: "1.15rem",
     },
-    "& p": {
-      color: "#444",
-      [theme.breakpoints.up("xs")]: {
-        fontSize: "1.15rem",
-      },
-      [theme.breakpoints.down("xs")]: {
-        fontSize: "1rem",
-      },
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "1rem",
     },
-    "& pre": {
-      color: "#444",
-      [theme.breakpoints.up("xs")]: {
-        fontSize: "1.15rem",
-      },
-      [theme.breakpoints.down("xs")]: {
-        fontSize: "1rem",
-      },
+  },
+  "& pre": {
+    color: "#444",
+    [theme.breakpoints.up("sm")]: {
+      fontSize: "1.15rem",
     },
-    "& li": {
-      color: "#444",
-      [theme.breakpoints.up("xs")]: {
-        fontSize: "1.15rem",
-      },
-      [theme.breakpoints.down("xs")]: {
-        fontSize: "1rem",
-      },
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "1rem",
     },
-    "& tr": {
-      color: "#444",
-      [theme.breakpoints.up("xs")]: {
-        fontSize: "1.15rem",
-      },
-      [theme.breakpoints.down("xs")]: {
-        fontSize: "1rem",
-      },
+  },
+  "& li": {
+    color: "#444",
+    [theme.breakpoints.up("sm")]: {
+      fontSize: "1.15rem",
+    },
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "1rem",
+    },
+  },
+  "& tr": {
+    color: "#444",
+    [theme.breakpoints.up("sm")]: {
+      fontSize: "1.15rem",
+    },
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "1rem",
     },
   },
 }))
@@ -115,15 +112,12 @@ const Post: React.FC<PageProps<DataProps>> = ({ data }) => {
   const { title, description } = data.markdownRemark.frontmatter
 
   return (
-    <StyledLayout>
+    <Layout>
       <Seo title={title} description={description} />
-      <div className={classes.post}>
-        <div
-          className={classes.body}
-          dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }}
-        />
-      </div>
-    </StyledLayout>
+      <PostMain>
+        <Body dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
+      </PostMain>
+    </Layout>
   )
 }
 
